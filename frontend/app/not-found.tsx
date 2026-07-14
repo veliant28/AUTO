438219cd9fe4619836e0dca266a08964fe2de74c
Veliant <veliant28@gmail.com>
2026-07-14 10:59:20 +0300

    chore: remove custom 404 pages, use Next.js default

--- frontend/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootNotFound() {
-  return (
-    <div style={{ textAlign: 'center', padding: '80px 20px', fontFamily: 'sans-serif' }}>
-      <h1 style={{ fontSize: '4rem', fontWeight: 'bold', margin: 0 }}>404</h1>
-      <p style={{ fontSize: '1.2rem', color: '#666' }}>Page not found</p>
-      <a href="/" style={{ color: '#0066cc', textDecoration: 'underline' }}>Go home</a>
-    </div>
-  );
-}
